109d64c1fb4dbbd40bd1026d19905021ed483c09
katsuikeda <katsuyukiikeda142857@gmail.com>
2024-12-19 02:18:16 -0800

    initialize Go module and implement basic HTTP server

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	const addr = ":8080"
+
+	mux.HandleFunc("/", handlePage)
+
+	srv := http.Server{
+		Handler:      mux,
+		Addr:         addr,
+		WriteTimeout: 30 * time.Second,
+		ReadTimeout:  30 * time.Second,
+	}
+
+	fmt.Println("server started on port", addr)
+	err := srv.ListenAndServe()
+	log.Fatal(err)
+}
+
+func handlePage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.Header().Set("Access-Control-Allow-Methods", "GET")
+	const page = `<html>
+<body>
+	<p> Hello from the go server </p>
+</body>
+</html>
+`
+	w.WriteHeader(200)
+	w.Write([]byte(page))
+}
